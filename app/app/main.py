38cf2dcb4d633ceb98f80f891c873e6f923b457a
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2021-01-29 10:28:08 +0100

    mendeley int

--- app/app/main.py
+++ app/app/main.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import requests
 import itertools
 import os
 import urllib
 from flask_socketio import SocketIO, emit, send
+from mendeley import Mendeley
+from mendeley.session import MendeleySession
+
+
+API_KEY = os.environ["API_KEY"]
+REDIRECT_URI = os.environ["REDIRECT_URI"]
+clientId = os.environ["MENDELEY_CLIENT_ID"]
+clientSecret = os.environ["MENDELEY_CLIENT_SECRET"]
+mendeley = Mendeley(clientId, clientSecret, REDIRECT_URI)
+SCPUS_BACKEND = f'https://api.elsevier.com/content/search/scopus?start=%d&count=%d&query=%s&apiKey={API_KEY}'
 
 app = Flask(__name__)
+app.secret_key = clientSecret
 socketio = SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
-API_KEY = os.environ["API_KEY"]
-
-SCPUS_BACKEND = f'https://api.elsevier.com/content/search/scopus?start=%d&count=%d&query=%s&apiKey={API_KEY}'
 
 
 @app.route("/robots.txt")
@@ -21,25 +29,29 @@ Disallow: /"""
 
 @app.route('/')
 @app.route('/home')
-@app.route('/query', methods=["GET"])
-def hello_world():
-    return render_template('index.html')
-
-
-@app.route('/query', methods=["POST"])
-def query():
-    the_query = request.form.get("query")
-
-    count, is_count, p = count_results_for_query(the_query)
-
-    print(f"fetching {count} results")
-    if is_count:
-        return render_template('index.html', query=the_query, count=count)
+def home():
+    if 'token' in session:
+        return render_template('index.html',token=session['token'])
     else:
-        refs = get_results_for_query(count, the_query)
-        dois = [r.get("prism:doi") for r in refs if "prism:doi" in r]
-        return render_template('dois.html', dois=dois)
+        auth = mendeley.start_authorization_code_flow()
+        session['state'] = auth.state
+        return render_template('index.html',login_url=auth.get_login_url())
 
+
+@app.route("/mendeleyLogout")
+def mendeleyLogout():
+    session.clear()
+    return redirect('/')
+
+@app.route('/oauth')
+def auth_return():
+    auth = mendeley.start_authorization_code_flow(state=session['state'])
+    mendeley_session = auth.authenticate(request.url)
+
+    session.clear()
+    session['token'] = mendeley_session.token
+
+    return redirect('/')
 
 def get_results_for_query(count, query):
     dois = []
@@ -92,6 +104,8 @@ def handle_get_dois(json_data):
 
     emit("dois", {"dois": dois})
 
+def get_session_from_cookies():
+    return MendeleySession(mendeley, session['token'])
 
 if __name__ == '__main__':
     app.config['SECRET_KEY'] = 'secret!'
