9d359a698b880b4dfe287e57d4d50528b25043b9
Nicolas Herbaut <nicolas.herbaut@gmail.com>
2023-04-05 16:43:20 +0200

    updated requirements

--- app/app/main.py
+++ app/app/main.py
@@ -9,35 +9,37 @@ from app.config import Config
 
 
 app = Flask(__name__)
-socketio = SocketIO(app)
-socketio.init_app(app, cors_allowed_origins="*")
-API_KEY = os.environ["API_KEY"]
-ROOT_URL = os.environ.get("ROOT_URL", "https://scholar.miage.dev")
-SHLINK_API_KEY = os.environ.get("SHLINK_API_KEY", "")
-REDIS_URL = os.environ.get("REDIS_URL", "")
-#MENDELEY_CLIENT_ID = os.environ.get("MENDELEY_CLIENT_ID", "")
-#MENDELEY_SECRET = os.environ.get("MENDELEY_SECRET", "")
 
-SCPUS_BACKEND = f'https://api.elsevier.com/content/search/scopus?start=%d&count=%d&query=%s&apiKey={API_KEY}'
-SCPUS_ABTRACT_BACKEND = f'https://api.elsevier.com/content/abstract/doi/%s?apiKey={API_KEY}'
-app.config.from_object(Config())
+with app.app_context():
+    socketio = SocketIO(app)
+    socketio.init_app(app, cors_allowed_origins="*")
+    API_KEY = os.environ["API_KEY"]
+    ROOT_URL = os.environ.get("ROOT_URL", "https://scholar.miage.dev")
+    SHLINK_API_KEY = os.environ.get("SHLINK_API_KEY", "")
+    REDIS_URL = os.environ.get("REDIS_URL", "")
+    #MENDELEY_CLIENT_ID = os.environ.get("MENDELEY_CLIENT_ID", "")
+    #MENDELEY_SECRET = os.environ.get("MENDELEY_SECRET", "")
+
+    SCPUS_BACKEND = f'https://api.elsevier.com/content/search/scopus?start=%d&count=%d&query=%s&apiKey={API_KEY}'
+    SCPUS_ABTRACT_BACKEND = f'https://api.elsevier.com/content/abstract/doi/%s?apiKey={API_KEY}'
+    app.config.from_object(Config())
 
 
 
 
-db = SQLAlchemy(app)
+    db = SQLAlchemy(app)
 
-if Config().IN_MEMORY:
-    print("### IN MEMORY DB")
-    db.create_all()
-    db.session.commit()
+    if Config().IN_MEMORY:
+        print("### IN MEMORY DB")
+        db.create_all()
+        db.session.commit()
 
-print("launching FLASK")
-# app.run(host="0.0.0.0")
-socketio = SocketIO(app)
-socketio.init_app(app, cors_allowed_origins="*")
-socketio.run(app, host="0.0.0.0")
-from app.rest import *
-from app.websocket import *
+    # print("launching FLASK")
+    # app.run(host="0.0.0.0")
+    socketio = SocketIO(app)
+    socketio.init_app(app, cors_allowed_origins="*")
+    socketio.run(app, host="0.0.0.0",allow_unsafe_werkzeug=True)
+    from app.rest import *
+    from app.websocket import *
 
-print("launching FLASK DONE")
+    print("launching FLASK DONE")
